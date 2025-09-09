01125ca0af307cb1b3e7b04d43588bcfbf6068d0
TanushBhargava <tanushbhargava1510@gmail.com>
2025-09-09 23:35:13 +0530

    Delete attendance-app directory

--- attendance-app/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AttendanceTable } from './AttendanceTable'
-export { default as AttendanceHistory } from './AttendanceHistory'
-export { default as FaceAttendance } from './FaceAttendance'
-
